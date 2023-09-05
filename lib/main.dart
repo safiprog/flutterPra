1bce8536d90acd4fe2cb7aa652022f92a5102bce
safi hamza <safihamzaprog7869@gmail.com>
2023-09-05 09:07:56 +0530

    day2 is successfully done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:safi_day1/home_page.dart';
 void main(){
   runApp(MyApp());
 
@@ -9,15 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: Material(
-        child:Center(
-        child: Container(
-          child: Text("saf is hero and programmer "),
-         
-        )
-        ),
-      ),
+    return const MaterialApp(
+      home: home_page()
     );
   }
 }
